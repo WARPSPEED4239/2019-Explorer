ab6bfab2162b8cfcfefc65dc458619161f01c97f
Miguel Garcia <miguelgarc5605@gmail.com>
2025-01-09 10:50:01 -0600

    Removing Unwanted Files

--- src/main/java/frc/robot/Constants.java
+++
@@ -1,7 +0,0 @@
-package frc.robot;
-
-public class Constants {
-
-    public static final boolean kCodeTestingState = false; //Note, setting this true enables: WristSetPositionWithJoystick, ElevatorSetPositionWithJoystick, ResetSensorsWithDPad, ElevatorLimitSwitches
-    public static final double kEpsilon = 0.0001;
-}
